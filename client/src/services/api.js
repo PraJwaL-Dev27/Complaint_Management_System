71e21ad0d54828661cfa9ae4239fd4248aa26c08
Prajwal Pathak <pathakprajwal675@gmail.com>
2026-05-19 12:20:11 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://complaint-management-server-17nb.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
